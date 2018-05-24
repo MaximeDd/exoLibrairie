330c51bd2206e1a0251bad01f651f9e5659d679a
Maxime.Dd <deniaud.maxime@gmail.com>
2018-05-24 18:53:57 +0200

    CRUD book

--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -1,9 +1,67 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Book} from '../models/book.model';
+import {Subject} from 'rxjs';
+import * as firebase from 'firebase';
+import DataSnapshot = firebase.database.DataSnapshot;
 
 @Injectable({
   providedIn: 'root'
 })
 export class BooksService {
 
-  constructor() { }
+  path = '/books';
+  books: Book[] = [];
+  booksSuject = new Subject<Book[]>();
+
+  emitBook() {
+    this.booksSuject.next(this.books);
+  }
+
+  getBooks() {
+    firebase.database()
+      .ref(this.path)
+      .on('value', (data: DataSnapshot) => {
+        const dataValue = data.val();
+        this.books = dataValue ? dataValue : [];
+        this.emitBook();
+      });
+  }
+
+  getBook(id: number) {
+    return new Promise(
+      (resolve, reject) => {
+        firebase.database()
+          .ref(this.path + id)
+          .once('value')
+          .then((data: DataSnapshot) => {
+            resolve(data.val());
+          }, (error) => {
+            reject(error);
+          });
+      }
+    );
+  }
+
+  saveBooks() {
+    firebase.database()
+      .ref(this.path)
+      .set(this.books);
+    this.emitBook();
+  }
+
+  createBook(newBook: Book) {
+    this.books.push(newBook);
+    this.saveBooks();
+  }
+
+  removeBook(oldBook: Book) {
+    const bookIndex = this.books.findIndex(
+      (book) => {
+        return book === oldBook;
+      }
+    );
+    this.books.splice(bookIndex, 1);
+    this.saveBooks();
+  }
+
 }
