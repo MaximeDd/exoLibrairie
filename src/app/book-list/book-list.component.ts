5c4623be621447bf0b0a2787fd6e05fb8b690331
Maxime.Dd <deniaud.maxime@gmail.com>
2018-05-24 20:37:04 +0200

    composants book-form, single-book et book-list

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Book} from '../models/book.model';
+import {Subscription} from 'rxjs';
+import {BooksService} from '../services/books.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.scss']
 })
-export class BookListComponent implements OnInit {
+export class BookListComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  books: Book[];
+  booksSubscription: Subscription;
 
-  ngOnInit() {
+  constructor(private booksService: BooksService,
+              private router: Router) {
   }
 
+  ngOnInit() {
+    this.booksService.getBooks();
+    this.booksSubscription = this.booksService.booksSuject.subscribe(
+      (books: Book[]) => {
+        this.books = books;
+      }
+    );
+    this.booksService.emitBook();
+  }
+
+  ngOnDestroy() {
+    this.booksSubscription.unsubscribe();
+  }
+
+  onAddBook() {
+    this.router.navigate(['/books', 'new']);
+  }
+
+  onDeleteBook(book: Book) {
+    this.booksService.removeBook(book);
+  }
+
+  onViewBook(id: number) {
+    this.router.navigate(['/books', 'view', id]);
+  }
 }
